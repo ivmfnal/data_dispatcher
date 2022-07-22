543f91cd10a7b7f754e470e19f6d6e7dbbe3d4a0
Igor Mandrichenko <ivm@fnal.gov>
2022-07-22 09:59:52 -0500

    .

--- samples/checksums.sh
+++ samples/checksums.sh
@@ -12,8 +12,10 @@ project_id=$1
 cert=${HOME}/certs/ivm@fnal.gov_cert.pem
 key=${HOME}/certs/ivm@fnal.gov_key.pem
 
-my_id=`dd worker id checksums_$$`
+my_id=worker_$$
 echo My id: $my_id
+tmpfile=/tmp/tmpdata_${my_id}.data
+info_file=/tmp/info_${my_id}.json
 
 cat << _EOF_ > /tmp/checksum.py
 import zlib, sys
@@ -27,19 +29,19 @@ _EOF_
 
 done="false"
 while [ $done == "false" ]; do
-	dd worker next -j $project_id > info.json
+	dd worker next -w $my_id -j $project_id > $info_file
         sts=$?
         #echo "dd worker next status:" $sts
         if [ "$sts" != "0" ]; then
 		done="true"
-                cat info.json
+                cat $info_file
+		rm -f $info_file
         else
-		url=`python ui/json_extract.py info.json replicas.0.url`
-		namespace=`python ui/json_extract.py info.json namespace`
-		name=`python ui/json_extract.py info.json name`
+		url=`python ui/json_extract.py $info_file replicas.0.url`
+		namespace=`python ui/json_extract.py $info_file namespace`
+		name=`python ui/json_extract.py $info_file name`
 		did=${namespace}:${name}
 
-                tmpfile=/tmp/tmpdata_$$
 
 		echo downloading $url
                 case $url in
@@ -58,9 +60,11 @@ while [ $done == "false" ]; do
 				;;
 		esac
                 ls -l $tmpfile
+                size=`stat -c %s $tmpfile`
 		checksum=`python /tmp/checksum.py < $tmpfile`
                 rm -f $tmpfile
-		echo $name : Adler32=$checksum
+		echo
+		echo "--->" $name: checksum=$checksum size=$size
                 echo
 		dd worker done $project_id $did
 	fi
