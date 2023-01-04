2fc085068b3475ab19c7b51a2e6f75465a4b22fa
Igor Mandrichenko <ivm@fnal.gov>
2023-01-03 19:31:35 -0500

    .

--- web_server/gui_server.py
+++ web_server/gui_server.py
@@ -140,6 +140,8 @@ class ProjectsHandler(BaseHandler):
         else:
             projects = list(DBProject.list(db, with_handle_counts=False, state='active'))
             
+        projects = sorted(projects, key=lambda p: p.ID)
+            
         nprojects = len(projects)
         npages = (nprojects + page_size - 1)//page_size
         projects = projects[istart:istart + page_size]
